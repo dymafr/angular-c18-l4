f672db90b3ad505e3c9f1559b1b15d99d3962b73
dymafr <erwan1309@hotmail.com>
2021-01-08 13:10:16 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from "@angular/animations";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("square", [
+      state(
+        "normal",
+        style({
+          backgroundColor: "white",
+          border: "2px solid #444",
+          borderRadius: "0"
+        })
+      ),
+      state(
+        "wild",
+        style({
+          backgroundColor: "red",
+          border: "none",
+          borderRadius: "50%"
+        })
+      ),
+      transition(":enter", [style({ backgroundColor: "blue" }), animate(1000)])
+    ])
+  ]
+})
+export class AppComponent implements OnInit {
+  public state = "normal";
+
+  ngOnInit() {}
+}
